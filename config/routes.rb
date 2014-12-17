d2f2e5f2f7873e66c24ef744611016110dff039c
Sean Eshbaugh <seaneshbaugh@gmail.com>
2014-12-17 15:01:45 -0600

    Updated some devise stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users, skip: [:sessions, :passwords, :registrations, :confirmations, :unlocks]
+
+  devise_scope :user do
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+
+    post 'update-password' => 'devise/passwords#create', as: :user_password
+    get 'reset-password' => 'devise/passwords#new', as: :new_user_password
+    get 'update-password' => 'devise/passwords#edit', as: :edit_user_password
+    put 'update-password' => 'devise/passwords#update'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
