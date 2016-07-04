cfa19a4472807123ad866b39d89ee1cfbc98a3cb
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-07-04 04:45:47 -0500

    Add title and meta description helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,20 @@
 module ApplicationHelper
+  def page_meta_description(meta_description)
+    if meta_description.present?
+      meta_description.strip
+    else
+      'CEDA Realty, reinventing the real estate experience!'
+    end
+  end
+
+  def page_title(title)
+    if title.present?
+      title.strip
+    else
+      'CEDA Realty'
+    end
+  end
+
   def present(object, klass = nil)
     klass ||= "#{object.class}Presenter".constantize
 
